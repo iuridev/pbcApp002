b3814400ad54511637e0ae6de87fc7854e18e1fd
iuridev <iuribarreto.ib@gmail.com>
2020-05-10 19:35:52 -0300

    config project

---
+++ backend/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingcomma: 'all',
+  arrowParend:"avoid"
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
+app.use(express.json)
 
-app.listen(3333,()=>{
-  console.log('server started!')
+app.listen(3333, () => {
+  console.log('server started!');
 });
